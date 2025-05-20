8896568c67f48a416867b7a0644a03515323d5cd
Nu55k0pF <madironbrain@googlemail.com>
2025-05-20 21:37:34 +0200

    starting to build reaper integration

--- osc_server.py
+++ osc_server.py
@@ -12,13 +12,17 @@ def print_handler(address, *args):
 def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
 
+def reaper_handler(address, *args):
+    print("now playing")
+
 
 dispatcher = Dispatcher()
-dispatcher.map("/something/*", print_handler)
-dispatcher.set_default_handler(default_handler)
+# dispatcher.map("/something/*", print_handler)
+dispatcher.map("/play", reaper_handler)
+# dispatcher.set_default_handler(reaper_handler)
 
 ip = "127.0.0.1"
-port = 5005
+port = 9000
 
 server = BlockingOSCUDPServer((ip, port), dispatcher)
 server.serve_forever()  # Blocks forever
