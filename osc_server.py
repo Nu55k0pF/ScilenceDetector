95324bf65e291973bf586839ef9d89e7b61fbcc0
Nu55k0pF <madironbrain@googlemail.com>
2025-05-20 22:50:24 +0200

    added typ annotation

--- osc_server.py
+++ osc_server.py
@@ -8,36 +8,37 @@ import pyaudio
 import audioop
 
 # Make all your configurations here
-CHUNK = 3200 # Sets buffe size
-BIT_DEPTH = pyaudio.paInt16
-CHANNELS = 1 # Mono or Stereo 1/2
-SAMPLING_RATE = 44100
-OSC_SERVER_IP = "127.0.0.1" # IP Adress of OSC server to send messages to
-OSC_SERVER_PORT = 8000
-OSC_ADRESS = "t/stop" # Enter OSC Adress to send message to
-OSC_VALUE =  "1"# Enter the desired value for your osc action
+CHUNK: int = 3200 # Sets buffe size
+BIT_DEPTH: int = pyaudio.paInt16
+CHANNELS: int = 1 # Mono or Stereo 1/2
+SAMPLING_RATE: int = 44100
+OSC_SERVER_IP: str = "127.0.0.1" # IP Adress of OSC server to send messages to
+OSC_SERVER_PORT: int = 8000
+OSC_ADRESS: str = "t/stop" # Enter OSC Adress to send message to
+OSC_VALUE: str =  "1"# Enter the desired value for your osc action
+HOST_IP: str = "127.0.0.1"
+HOST_PORT: int = 9000
 
-def send_osc_message():
+def send_osc_message() -> None:
     """Use this to configure the action to take when detecitng scilence"""
     osc_client = udp_client.SimpleUDPClient(address=OSC_SERVER_IP, port=OSC_SERVER_PORT) 
-    print("Sending {} '{}' to {}:{}".format(OSC_ADRESS, OSC_VALUE, OSC_SERVER_IP, OSC_SERVER_PORT ))
+    print("Sending {} '{}' to {}:{}".format(OSC_ADRESS, OSC_VALUE, OSC_SERVER_IP, OSC_SERVER_PORT))
     osc_client.send_message(OSC_ADRESS, value=OSC_VALUE) #Send a OSC Message to some other hardware or module
 
-def detect_scilence(stream):
+def detect_scilence(stream: PyAudio.Stream) -> None:
     # Initialize an arry to store data in
-    frames = [] 
-    # how long to record
-    seconds = 6 
+    frames: list = [] 
     # stors the last x rms values
-    last_rms = [] 
-    threshold = 50
+    last_rms: list = [] 
+    threshold: int = 50
     # at 44.1 kHz and 3200 samples in the buffer you have about 14 chunks per second, so if you want a detection time of 2 seconds 30 is about the right value here. 3 sek ~ 42
-    time = 40 
-    listen = True
+    time: int = 40 
+
+    listen: bool = True
     while listen:
         data = stream.read(CHUNK)
         frames.append(data)
-        rms = audioop.rms(data, 2)
+        rms: int = audioop.rms(data, 2)
         
         if len(last_rms) < time:
             last_rms.append(rms)
@@ -46,7 +47,7 @@ def detect_scilence(stream):
             last_rms.pop(0)
             last_rms.append(rms)
             
-            avarage = sum(last_rms)/len(last_rms)
+            avarage: float = sum(last_rms)/len(last_rms)
             if avarage > threshold:
                 print(rms)
                 continue
@@ -54,8 +55,9 @@ def detect_scilence(stream):
                 send_osc_message()
                 listen = False
 
-def handler(address, *args):
-    """This is an example function. It detects the Reaper OSC message for toggle play and starts listening. 
+def handler(address: str, *args: list[str]):
+    """This is an example function. 
+    It detects the Reaper OSC message for toggle play and starts listening. 
     This needs to be adapted for other aplications
     """
     print("Recived {} {}".format(address, args))
@@ -78,8 +80,5 @@ dispatcher = Dispatcher()
 dispatcher.map("/play", handler)
 # dispatcher.set_default_handler(reaper_handler)
 
-ip = "127.0.0.1"
-port = 9000
-
-server = BlockingOSCUDPServer( (ip, port), dispatcher)
+server = BlockingOSCUDPServer( (HOST_IP, HOST_PORT), dispatcher)
 server.serve_forever()  # Blocks forever
