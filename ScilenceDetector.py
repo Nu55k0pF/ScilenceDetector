2a906d5dca7fadb0106195eb6ffb6419ebbe8680
Nu55k0pF <madironbrain@googlemail.com>
2025-05-20 22:20:22 +0200

    now used for testing

--- ScilenceDetector.py
+++ ScilenceDetector.py
@@ -16,6 +16,39 @@ def get_audio_device_by_name():
     for i in range(0, p.get_device_count()):    
         print(i, p.get_device_info_by_index(i)['name'])
 
+def detect_scilence(stream):
+    # Initialize an arry to store data in
+    frames = [] 
+    # how long to record
+    seconds = 6 
+    # stors the last x rms values
+    last_rms = [] 
+    threshold = 100
+    # at 44.1 kHz and 3200 samples in the buffer you have about 14 chunks per second, so if you want a detection time of 2 seconds 30 is about the right value here. 3 sek ~ 42
+    time = 40 
+    listen = True
+    while listen:
+        data = stream.read(CHUNK)
+        frames.append(data)
+        rms = audioop.rms(data, 2)
+        
+        if len(last_rms) < time:
+            last_rms.append(rms)
+            print(rms)
+        else:
+            last_rms.pop(0)
+            last_rms.append(rms)
+            
+            avarage = sum(last_rms)/len(last_rms)
+            if avarage > threshold:
+                print(rms)
+                continue
+            else:
+                print("Scilence Detected!!!!")
+                print("Sending {} to {}:{}{}".format(OSC_VALUE, OSC_SERVER_IP, OSC_SERVER_PORT, OSC_ADRESS))
+                osc_client.send_message(OSC_ADRESS, value=OSC_VALUE) #Send a OSC Message to some other hardware or module
+                listen = False
+
 # Make all your configurations here
 CHUNK = 3200 # Sets buffe size
 BIT_DEPTH = pyaudio.paInt16
@@ -23,8 +56,8 @@ CHANNELS = 1 # Mono or Stereo 1/2
 SAMPLING_RATE = 44100
 OSC_SERVER_IP = "127.0.0.1" # IP Adress of OSC server to send messages to
 OSC_SERVER_PORT = 8000
-OSC_ADRESS = "t/play" # Enter OSC Adress to send message to
-OSC_VALUE = 1 # Enter the desired value for your osc action
+OSC_ADRESS = "s/marker/1/name" # Enter OSC Adress to send message to
+OSC_VALUE =  "Scilence detected"# Enter the desired value for your osc action
 
 # Initialize the osc_clienat
 osc_client = udp_client.SimpleUDPClient(address=OSC_SERVER_IP, port=OSC_SERVER_PORT) 
@@ -45,39 +78,9 @@ stream = p.open(
     input_device_index=device_index # here you have to set the input device to monitor use p.get_device_info_by_index() to list all available
 )
 
-# Initialize an arry to store data in
-frames = [] 
-# how long to record
-seconds = 6 
-# stors the last x rms values
-last_rms = [] 
-threshold = 100
-# at 44.1 kHz and 3200 samples in the buffer you have about 14 chunks per second, so if you want a detection time of 2 seconds 30 is about the right value here. 3 sek ~ 42
-time = 40 
-listen = True
 
 # for i in range(0, int(SAMPLING_RATE/CHUNK * seconds)):
-while listen:
-    data = stream.read(CHUNK)
-    frames.append(data)
-    rms = audioop.rms(data, 2)
-    
-    if len(last_rms) < time:
-        last_rms.append(rms)
-        print(rms)
-    else:
-        last_rms.pop(0)
-        last_rms.append(rms)
-        
-        avarage = sum(last_rms)/len(last_rms)
-        if avarage > threshold:
-            print(rms)
-            continue
-        else:
-            print("Scilence Detected!!!!")
-            print("Sending {} to {}:{}{}".format(OSC_VALUE, OSC_SERVER_IP, OSC_SERVER_PORT, OSC_ADRESS))
-            osc_client.send_message(OSC_ADRESS, value=OSC_VALUE) #Send a OSC Message to some other hardware or module
-            listen = False
+detect_scilence(stream)
 
 # Stop and Close the audiostream
 stream.start_stream()
