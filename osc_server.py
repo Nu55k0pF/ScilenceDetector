7bbf683afe2227b47fb80f47a70f6a897510b79f
Nu55k0pF <madironbrain@googlemail.com>
2025-05-22 20:51:41 +0200

    worked on config section

--- osc_server.py
+++ osc_server.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import queue
 import sys
+import time
 
 # Set environment variable before importing sounddevice. Value is not important.
 os.environ["SD_ENABLE_ASIO"] = "1"
@@ -14,25 +15,44 @@ os.environ["SD_ENABLE_ASIO"] = "1"
 import sounddevice as sd
 
 
-# Make all your configurations here
+"""The followoing part of code is to be configured as required
+Don't mess with anything after the config section if you are just using this.
+If you are not shure what device ID your audio hardware has or what sampling rate to use
+set DEFAULT = True
+"""
+# Audio device config
+DEFAULT = False
 CHUNK: int = 3200
 CHANNELS = 1 # Focusrite 18i8 loopback Channels 11/12
-SAMPLING_RATE: int = 44100
-DEVICE = 3
-OSC_SERVER_IP: str = "127.0.0.1" # IP Adress of OSC server to send messages to
-OSC_SERVER_PORT: int = 8000
+SAMPEL_RATE: int = 44100
+DEVICE = 3 # Enter 'None' here if you just want to use your default audio device 
+
+if DEFAULT:
+    DEVICE = sd.default.device
+    SAMPEL_RATE = sd.query_devices(DEVICE, 'input')['default_samplerate']
+
+# Network and OSC config
+REMOTE_OSC_IP: str = "127.0.0.1" # IP Adress of OSC server to send messages to
+REMOTE_OSC_PORT: int = 8000
+LOCAL_OSC_IP: str = "127.0.0.1"
+LOCAL_OSC_PORT: int = 9000
+
+# Scilence detection config
+SCILENCE_DETECT_LEVEL: float = -50
+SECONDS: int = 3 # This number reperesents the time in seconds
+SCILENCE_DETECT_TIME: int = round(SAMPEL_RATE/CHUNK * SECONDS)
+
+# Failover OSC acction config
 OSC_ADRESS: str = "t/stop" # Enter OSC Adress to send message to
 OSC_VALUE: str =  "1" # Enter the desired value for your osc action
-HOST_IP: str = "127.0.0.1"
-HOST_PORT: int = 9000
 
 q = queue.Queue()
 
 def send_osc_message() -> None:
     """Use this to configure the action to take when detecitng scilence"""
-    osc_client = udp_client.SimpleUDPClient(address=OSC_SERVER_IP, port=OSC_SERVER_PORT) 
-    print("Sending {} '{}' to {}:{}".format(OSC_ADRESS, OSC_VALUE, OSC_SERVER_IP, OSC_SERVER_PORT))
-    osc_client.send_message(OSC_ADRESS, value=OSC_VALUE) #Send a OSC Message to some other hardware or module
+    osc_client = udp_client.SimpleUDPClient(address=REMOTE_OSC_IP, port=REMOTE_OSC_PORT) 
+    print("Sending {} '{}' to {}:{}".format(OSC_ADRESS, OSC_VALUE, REMOTE_OSC_IP, REMOTE_OSC_PORT))
+    osc_client.send_message(OSC_ADRESS, value=OSC_VALUE) # Send a OSC Message to some other hardware or module
 
 
 def calculate_dbfs(data) -> float:
@@ -45,22 +65,19 @@ def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
     if status:
         print(status, file=sys.stderr)
-    # put indata in the cue to be used in the detect_scilence function
+    # Put indata in the cue to be used in the detect_scilence function
     q.put(indata.copy())
 
 
 def detect_scilence():
-    # Initialize an arry to store data in
-    frames = [] 
-    # stors the last x dbfs values
+    # Stors the last x dbfs values
     last_messurment = [] 
     threshold = -80
     # at 44.1 kHz and 3200 samples in the buffer you have about 14 chunks per second, so if you want a detection time of 2 seconds 30 is about the right value here. 3 sek ~ 42
-    time = 40 
+    time = SCILENCE_DETECT_TIME 
     listen = True
     while listen:
         frame = q.get()
-        frames.append(frame)
         dbfs = calculate_dbfs(frame)
 
         if len(last_messurment) < time:
@@ -84,19 +101,23 @@ def handler(address: str, *args: list[str]):
     """This is an example function. 
     It detects the Reaper OSC message for toggle play and starts listening. 
     This needs to be adapted for other aplications
+    map the OSC adress for the dispatcher according to your DAW or other OSC device
+
+    addres: OSC adress like /play
+    *args: OSC values 1 or 0
     """
     print("Recived {} {}".format(address, args))
     if args [-1] == 1:
-        stream = sd.InputStream(samplerate=SAMPLING_RATE, channels=CHANNELS,
+        stream = sd.InputStream(samplerate=SAMPEL_RATE, channels=CHANNELS,
             blocksize=CHUNK, device=20, callback=callback)
         with stream:
             detect_scilence()
     else:
         return
 
-
+print(SAMPEL_RATE, DEVICE)
 dispatcher = Dispatcher()
 dispatcher.map("/play", handler)
 
-server = BlockingOSCUDPServer((HOST_IP, HOST_PORT), dispatcher)
+server = BlockingOSCUDPServer((LOCAL_OSC_IP, LOCAL_OSC_PORT), dispatcher)
 server.serve_forever()  # Blocks forever
