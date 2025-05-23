d5610c3a220e0794472706621831dc90fd89cb15
Nu55k0pF <madironbrain@googlemail.com>
2025-05-23 11:57:19 +0200

    added multithreading

--- automatic_failover.py
+++ automatic_failover.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import queue
 import sys
+import threading
 import time
 
 # Set environment variable before importing sounddevice. Value is not important.
@@ -38,7 +39,7 @@ LOCAL_OSC_PORT: int = 11001
 SCILENCE_DETECT_LEVEL: float = -50
 SECONDS: int = 3 # This number reperesents the time in seconds
 SCILENCE_DETECT_TIME: int = round(SAMPEL_RATE/CHUNK * SECONDS)
-THRESHOLD: float = -500 # In DBFS
+THRESHOLD: float = -50 # In DBFS
 
 # Failover OSC acction config
 OSC_ADRESS: str = "t/stop" # Enter OSC Adress to send message to
@@ -55,6 +56,8 @@ Don't mess with anything after the config section if you just want to use the pr
 #     CHANNELS = sd.default.channels
 
 q = queue.Queue()
+stop_event = threading.Event()
+detection_thread = None  # Globaler Thread-Handle
 
 def send_osc_message() -> None:
     """Use this to configure the action to take when detecitng scilence"""
@@ -78,22 +81,19 @@ def callback(indata, frames, time, status):
 
 
 def detect_scilence() -> None:
-    # Stors the last x dbfs values
     last_messurment: list = [] 
     threshold = THRESHOLD
-    time = SCILENCE_DETECT_TIME 
-    listen = True
-    #TODO: add functionality to dect when playback is stopped manually
-    while listen:
+    time_ = SCILENCE_DETECT_TIME 
+    stop_event.clear()
+    while not stop_event.is_set():
         frame = q.get()
         dbfs = calculate_dbfs(frame)
-        if len(last_messurment) < time:
+        if len(last_messurment) < time_:
             last_messurment.append(dbfs)
             print(dbfs)
         else:
             last_messurment.pop(0)
             last_messurment.append(dbfs)
-            
             avarage = sum(last_messurment)/len(last_messurment)
             if avarage > threshold:
                 print(dbfs)
@@ -102,7 +102,7 @@ def detect_scilence() -> None:
                 print("Scilence Detected!!!!")
                 print(avarage, threshold)
                 send_osc_message()
-                listen = False
+                break
 
 
 def handler(address: str, *args: list[str]):
@@ -110,22 +110,44 @@ def handler(address: str, *args: list[str]):
     It detects the Reaper OSC message for toggle play and starts listening. 
     This needs to be adapted for other aplications
     map the OSC adress for the dispatcher according to your DAW or other OSC device.
-    By default this script assumes, that it gets '/play 1' as its OSC start signal 
+    By default this script assumes, that it gets '/play (1, 1)' as its OSC start signal 
 
     addres: OSC adress like /play
     *args: OSC values 1 or 0
     """
     print("Recived {} {}".format(address, args))
-    if args [-1] == 1:
-        stream = sd.InputStream(samplerate=SAMPEL_RATE, channels=CHANNELS,
-            blocksize=CHUNK, device=DEVICE, callback=callback)
-        with stream:
-            detect_scilence()
-    else:
+    if args and args[-1] == 0:
+        stop_event.set()
         return
+    if args and args[-1] == 1:
+        start_detection()
 
-dispatcher = Dispatcher()
-dispatcher.map("/play", handler)
 
-server = BlockingOSCUDPServer((LOCAL_OSC_IP, LOCAL_OSC_PORT), dispatcher)
-server.serve_forever()  # Blocks forever
+def start_detection():
+    global detection_thread
+    if detection_thread is None or not detection_thread.is_alive():
+        stop_event.clear()
+        detection_thread = threading.Thread(target=run_detection, daemon=True)
+        detection_thread.start()
+
+
+def run_detection():
+    with sd.InputStream(samplerate=SAMPEL_RATE, channels=CHANNELS,
+                        blocksize=CHUNK, device=DEVICE, callback=callback):
+        detect_scilence()
+
+
+def osc_server_thread():
+    dispatcher = Dispatcher()
+    dispatcher.map("/play", handler)
+    # Optional: dispatcher.map("/stop", handler)
+    server = BlockingOSCUDPServer((LOCAL_OSC_IP, LOCAL_OSC_PORT), dispatcher)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    t = threading.Thread(target=osc_server_thread, daemon=True)
+    t.start()
+    print("OSC Server läuft. Warte auf /play ...")
+    while True:
+        time.sleep(1)
