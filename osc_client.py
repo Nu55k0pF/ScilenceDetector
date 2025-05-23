f91d405271e67e096d26364a71d4fccf12e0fdcc
Nu55k0pF <madironbrain@googlemail.com>
2025-05-23 12:12:23 +0200

    added todo

--- osc_client.py
+++ osc_client.py
@@ -1,9 +1,11 @@
+#TODO: Programm so anpassen, dass man es über die Konsole mit argparser starten kann um testnachrichten zu senden
+
 from pythonosc.udp_client import SimpleUDPClient
 
-ip = "192.168.1.4"
-port = 1337
+ip = "10.10.0.83"
+port = 11001
 
 client = SimpleUDPClient(ip, port)  # Create client
 
-client.send_message("/live/song/get/tempo", 1)   # Send float message
+client.send_message("/play", (1, 1))    # Send float message
 # client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
